f77ec18865454068fa432c437e8e132b0fc58d24
MMark4 <mmarca@pm.me>
2023-02-23 14:24:36 +0200

    update files

---
+++ src/step-definitions/click.ts
@@ -0,0 +1,35 @@
+import { When } from '@cucumber/cucumber';
+import { ScenarioWorld } from './setup/world';
+import {
+    clickElement
+} from '../support/html-behaviour';
+import { waitFor } from '../support/wait-for-behaviour';
+import { getElementLocator } from '../support/web-element-helper';
+import { ElementKey } from '../env/global';
+
+When(
+    /^I click the "([^"]*)" (?:button|link|icon|element)$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`I click the ${elementKey} (?:button|link|icon|element)`)
+        
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async () => {
+            
+            const result = await page.waitForSelector(elementIdentifier, {
+                state: 'visible',
+            });
+
+            if (result) {
+                await clickElement(page, elementIdentifier);
+            };
+
+            return result;
+        });
+    }
+)
